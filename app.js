2dbae993df502e657ccb3d1c6bd426df6340119f
Wahyu Wiyoko <137708513+wahyuwiyoko@users.noreply.github.com>
2023-10-10 14:36:01 +0800

    Add more `alt` attribute to `img`

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const membersUrl = "https://api.github.com/orgs/you-apps/members";
 const fetchJson = async (url) => {
   const resp = await fetch(url);
   return await resp.json();
-}
+};
 
 const loadRepos = async () => {
   const repos = await fetchJson(reposUrl);
@@ -16,30 +16,30 @@ const loadRepos = async () => {
 
     let newHTML = `
       <a class="card" href="${repo.html_url}">
-        <img src="https://raw.githubusercontent.com/you-apps/${repo.name}/main/fastlane/metadata/android/en-US/images/icon.png">
+        <img src="https://raw.githubusercontent.com/you-apps/${repo.name}/main/fastlane/metadata/android/en-US/images/icon.png" alt="${repo.name} icon">
         <div>
           <h3>${repo.name}</h3>
           <p>${repo.description}</p>
         </div>
-        <span class="stars"><img src="assets/star.svg">${repo.stargazers_count}</span>
+        <span class="stars"><img src="assets/star.svg" alt="Star icon">${repo.stargazers_count}</span>
       </a>
     `;
     $("#apps > div").innerHTML += newHTML;
   }
-}
+};
 
 const loadMembers = async () => {
   const members = await fetchJson(membersUrl);
   for (let member of members) {
     let newHTML = `
-      <a class="card" href=${member.html_url}>
-        <img src=${member.avatar_url}>
+      <a class="card" href="${member.html_url}">
+        <img src="${member.avatar_url}" alt="${member.login} avatar">
         <h3>${member.login}</h3>
       </a>
     `;
     $("#team > div").innerHTML += newHTML;
   }
-}
+};
 
 loadRepos();
 loadMembers();
