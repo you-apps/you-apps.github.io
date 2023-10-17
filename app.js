ed6db4ee665b0f2a22b35b35f1af2716f7223668
abrocks1120@gmail.com <abrocks1120@gmail.com>
2023-10-17 14:00:42 +0530

    replace innerHTML with createElement & fix sorting

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-let element = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
+const x = document.createElement.bind(document);
 
 const reposUrl = "https://api.github.com/orgs/you-apps/repos";
 const membersUrl = "https://api.github.com/orgs/you-apps/members";
@@ -12,38 +13,51 @@ async function fetchJson(url) {
 
 async function loadRepos() {
   const repos = await fetchJson(reposUrl);
-  repos.sort((a, b) => a.stargazers_count < b.stargazers_count);
+  repos
+    .filter(repo => repo.name.includes("You"))
+    .sort((a, b) => b.stargazers_count - a.stargazers_count)
+    .map(repo => {
+      
+      const a = x("a");
+      a.className = "card";
+      a.href = repo.html_url;
 
-  for (const repo of repos) {
-    if (!repo.name.includes("You")) continue;
+      const img = x("img");
+      img.src = `https://raw.githubusercontent.com/you-apps/${repo.name}/main/fastlane/metadata/android/en-US/images/icon.png`;
 
-    const apps = `
-      <a class="card" href="${repo.html_url}">
-        <img src="https://raw.githubusercontent.com/you-apps/${repo.name}/main/fastlane/metadata/android/en-US/images/icon.png" alt="${repo.name} icon">
-        <div>
-          <h3>${repo.name}</h3>
-          <p>${repo.description}</p>
-        </div>
-        <span class="stars"><img src="assets/star.svg" alt="Star icon">${repo.stargazers_count}</span>
-      </a>
-    `;
+      const div = x("div");
+      const h3 = x("h3");
+      h3.textContent = repo.name;
+      const p = x("p");
+      p.textContent = repo.description;
+      div.append(h3, p);
 
-    element("#app-list").innerHTML += apps;
-  }
+      const span = x("span");
+      span.className = "stars";
+      const starIcon = x("img");
+      starIcon.src = "assets/star.svg";
+      span.append(starIcon, repo.stargazers_count);
+
+      a.append(img, div, span);
+      $("#apps > div").appendChild(a);
+    });
 }
 
 async function loadMembers() {
   const members = await fetchJson(membersUrl);
-
   for (const member of members) {
-    const team = `
-      <a class="card" href="${member.html_url}">
-        <img src="${member.avatar_url}" alt="${member.login} avatar">
-        <h3>${member.login}</h3>
-      </a>
-    `;
-
-    element("#member-list").innerHTML += team;
+    const a = x("a");
+    a.className = "card";
+    a.href = member.html_url;
+    
+    const img = x("img");
+    img.src = member.avatar_url;
+    
+    const h3 = x("h3");
+    h3.textContent = member.login;
+    
+    a.append(img, h3);
+    $("#team > div").appendChild(a);
   }
 }
 
